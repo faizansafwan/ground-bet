71d62211b4710c6a3c8d54bc4f2332e2fa68ec60
faizansafwan <faizansafwan075@gmail.com>
2025-06-29 19:16:30 +0530

    permission for frontend

--- ground-bet-api/src/server.js
+++ ground-bet-api/src/server.js
@@ -10,7 +10,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://faizansafwan.github.io', // frontend GitHub Pages
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  }));
 app.use(express.json());
 
 app.use('/api/bets', betRoutes);
