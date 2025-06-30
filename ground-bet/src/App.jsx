eb4884d688829ce22eac6728c9ba5c94e6cd0871
faizansafwan <faizansafwan075@gmail.com>
2025-06-30 17:09:14 +0530

    pop up for sucess and error

--- ground-bet/src/App.jsx
+++ ground-bet/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import Dashboard from './pages/dashboard'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +10,7 @@ function App() {
   return (
     <>
       <Dashboard />
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
     </>
   )
 }
