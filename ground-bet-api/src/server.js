cbe96583bc4cfcab7a64b8bc0b30c299032cfff1
faizansafwan <faizansafwan075@gmail.com>
2025-06-30 16:40:11 +0530

    permission for local frontend

--- ground-bet-api/src/server.js
+++ ground-bet-api/src/server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'https://faizansafwan.github.io', // frontend GitHub Pages
+    origin: ['https://faizansafwan.github.io', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
   }));
